ad555c74286965e148fd90ac0d5c4684f4571134
Ha_Tan_Luc <hatanluc0@outlook.fr>
2023-10-27 15:50:01 +0200

    new key

--- models/connection.js
+++ models/connection.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // Définir le lien de connexion MongoDB
 const mongoURI = process.env.MONGODB_URI;
 
-// Se connecter à MongoDB
+
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('Connected to MongoDB successfully!');
-    // Votre code ici une fois la connexion établie
+    console.log('Connexion à MongoDB établie avec succès');
   })
-  .catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
+  .catch((error) => {
+    console.error('Erreur de connexion à MongoDB :', error);
   });
